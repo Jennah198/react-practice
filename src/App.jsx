34de108284387e18f0f1b357ef9e3cb015e919c4
Nezira <neziraworku198@gmail.com>
2026-04-21 15:00:26 -0700

    added fotter component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './components/Header'
+import Fotter from './components/Fotter'
 
 function App() {
   return (
     <div>
       <Header />
+      <Fotter />
     </div>
   )
 }
