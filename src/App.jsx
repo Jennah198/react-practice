0456587ef2f627a5376c2aad8d28712b2c7d28af
Nezira <neziraworku198@gmail.com>
2026-04-21 15:05:32 -0700

    added Alert Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './components/Header'
 import Fotter from './components/Fotter'
+import Alert from './components/Alert'
 
 function App() {
   return (
     <div>
       <Header />
+      <Alert />
       <Fotter />
     </div>
   )
