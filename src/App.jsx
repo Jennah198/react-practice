e07873a5d1148419cc2c72f69be80676913c4af6
Nezira <neziraworku198@gmail.com>
2026-04-21 15:12:15 -0700

    added first section component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from './components/Header'
 import Fotter from './components/Fotter'
 import Alert from './components/Alert'
+import FirstSection from './components/FirstSection'
 
 function App() {
   return (
     <div>
       <Header />
       <Alert />
+      <FirstSection />
       <Fotter />
     </div>
   )
