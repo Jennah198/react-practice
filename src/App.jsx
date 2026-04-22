48daa29452a24b1351e80a280bb4885a27522cba
Nezira <neziraworku198@gmail.com>
2026-04-22 04:35:07 -0700

    added fourth section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Alert from './components/Alert'
 import FirstSection from './components/FirstSection'
 import SecondSection from './components/SecondSection'
 import ThirdSection from './components/ThirdSection'
+import FourthSection from './components/FourthSection'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <FirstSection />
       <SecondSection />
       <ThirdSection />
+      <FourthSection />
       <Fotter />
     </div>
   )
